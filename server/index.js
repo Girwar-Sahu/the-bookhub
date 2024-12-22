3f43ce79ae61d7f720c3a368345dc623567c2c85
Girwar-Sahu <girvar786sahu@gmail.com>
2024-12-22 16:56:43 +0530

    some changes in file structure

--- server/index.js
+++ server/index.js
@@ -1,16 +1,25 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./db.config.js";
 import bookRouter from "./routes/book.route.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/books", bookRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
