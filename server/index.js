234d1394398a2fafacfdf8ea0cca7abb077e1af2
Girwar-Sahu <girvar786sahu@gmail.com>
2024-12-22 14:02:45 +0530

    configure database for server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db.config.js";
+import bookRouter from "./routes/book.route.js";
+
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use(cors());
+app.use(express.json());
+app.use("/api/books", bookRouter);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running on port ${PORT}`);
 });
