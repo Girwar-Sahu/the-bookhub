49060177d55802c54477bb745c70a5f7100a949c
Girwar-Sahu <girvar786sahu@gmail.com>
2024-12-24 23:38:15 +0530

    some changes

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "mern-blog-d1c6e.firebaseapp.com",
   projectId: "mern-blog-d1c6e",
   storageBucket: "mern-blog-d1c6e.appspot.com",
