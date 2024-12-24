d79a88075bb9f2badab7696e750446326deb6a91
Girwar-Sahu <girvar786sahu@gmail.com>
2024-12-24 23:04:38 +0530

    update recent book component

--- client/src/components/RecommendedBooks.jsx
+++ client/src/components/RecommendedBooks.jsx
@@ -1,35 +1,15 @@
-import React from "react";
-import { Card, CardContent, CardFooter } from "./ui/card";
+import React, { useEffect } from "react";
 import { Button } from "./ui/button";
-import { ScrollArea, ScrollBar } from "./ui/scroll-area";
-const books = [
-  {
-    id: 1,
-    title: "The Alchemist",
-    author: "Paulo Coelho",
-    description: "lorem ipsum",
-  },
-  {
-    id: 2,
-    title: "The Titanic",
-    author: "Paulo Coelho",
-    description: "lorem ipsum",
-  },
-  {
-    id: 3,
-    title: "The Alchemist",
-    author: "Paulo Coelho",
-    description: "lorem ipsum",
-  },
-  {
-    id: 4,
-    title: "The Alchemist",
-    author: "Paulo Coelho",
-    description: "lorem ipsum",
-  },
-];
+import { useBookContext } from "@/utils/BookContext";
+import { Link } from "react-router-dom";
 
 const RecommendedBooks = ({ title }) => {
+  const { fetchBooks, books } = useBookContext();
+
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
   return (
     <section className="py-4 w-full dark:bg-gray-900">
       <div className="max-w-7xl mx-auto">
@@ -37,27 +17,38 @@ const RecommendedBooks = ({ title }) => {
           {title}
         </h1>
 
-        <div className="w-3/4 md:w-full mx-auto mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 md:gap-6 border dark:border-gray-700 boder-gray-200 px-4 rounded-lg">
-          {books.map((book) => (
-            <Card
-              key={book.id}
-              className="dark:bg-gray-800 dark:text-white mx-auto my-5 shadow-lg border dark:border-gray-700 boder-gray-200"
+        <div className="w-3/4 md:w-full mx-auto mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6 px-4 rounded-lg">
+          {books.slice(0, 5).map((book) => (
+            <div
+              key={book._id}
+              className="w-full mx-auto my-5 shadow-lg rounded-lg bg-white dark:bg-gray-800 dark:text-white overflow-hidden border dark:border-gray-700 p-2"
             >
-              <CardContent className="flex flex-col items-center">
+              {/* Image Section */}
+              <div className="w-full h-64 overflow-hidden">
                 <img
-                  className="w-full object-contain"
-                  src="/booklogo.png"
+                  className="w-full h-full object-cover"
+                  src={book.imageURL}
                   alt={book.title}
                 />
-                <h2 className="text-xl font-semibold">{book.title}</h2>
-                <p className="text-sm text-gray-500">{book.author}</p>
-              </CardContent>
-              <CardFooter className="flex flex-col items-center">
-                <Button className="mt-2 w-2/3 bg-sky-500 hover:bg-sky-600 dark:bg-sky-700 dark:text-white dark:hover:bg-sky-600">
-                  View Details
-                </Button>
-              </CardFooter>
-            </Card>
+              </div>
+
+              {/* Content Section */}
+              <div className="p-4">
+                <h2 className="text-xl font-semibold mt-4">{book.title}</h2>
+                <p className="text-sm text-gray-500 dark:text-gray-300">
+                  {book.author}
+                </p>
+              </div>
+
+              {/* Footer Section */}
+              <div className="p-4 bg-gray-100 dark:bg-gray-700 text-center">
+                <Link to={`/book/${book._id}`}>
+                  <Button className="w-full bg-sky-500 hover:bg-sky-600 dark:bg-sky-700 dark:text-white dark:hover:bg-sky-600">
+                    View Details
+                  </Button>
+                </Link>
+              </div>
+            </div>
           ))}
         </div>
       </div>
